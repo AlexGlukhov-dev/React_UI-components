6e6aea430cd333910ca105f1a663789677876505
Vladinero <vlad.servicemail@gmail.com>
2022-05-13 12:10:32 +0300

    Added modal component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import {useState} from "react";
+
 import Tabs from "./UI/Tabs/Tabs";
 
 import tabsInfo from './DB/TABS_DATA.json'
 
 import './App.css';
+import Modal from "./UI/Modal";
 
 function App() {
+  const [modalActive, setModalActive] = useState(true)
   return (
     <div className="App">
       <div  className="container">
+        <section>
+          <h3>Tabs component</h3>
         <Tabs tabsInfo={tabsInfo}/>
+        </section>
+        <section>
+          <h3>Modal(popup) component</h3>
+          <button onClick={() => setModalActive(true)}>Open modal</button>
+          <Modal active={modalActive} setActive={setModalActive}>
+            <p>Place content here</p>
+          </Modal>
+        </section>
+
       </div>
     </div>
   );
--- src/UI/Modal/index.js
+++ src/UI/Modal/index.js
@@ -1,9 +1,20 @@
-import classes from "modal.module.scss";
+import cn from 'classnames';
 
-const Modal = ({children, ...props}) => {
+
+import classes from "./modal.module.scss";
+
+const Modal = ({active, setActive, children}) => {
     return (
-        <div className={classes["modal"]} {...props}>
-            {children}
+        <div className={cn(classes["modal"], active && classes["active"] )} onClick={() => setActive(false)}>
+            <div className={cn(classes["modal__content"],  active && classes["active"])} onClick={e => e.stopPropagation()}>
+              <button className={classes["modal__closeBtn"]} onClick={() => setActive(false)}>
+                <svg width="43" height="43" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <rect x="9.90039" y="31.1128" width="30" height="1" transform="rotate(-45 9.90039 31.1128)" fill="black"/>
+                  <rect x="11.3145" y="9.89966" width="30" height="0.999999" transform="rotate(45 11.3145 9.89966)" fill="black"/>
+                </svg>
+              </button>
+              {children}
+            </div>
         </div>
     );
 };
